b2b39c0d126ec0136b76fa82b84ab4d9f95a2b76
whylele <163873282+whylele@users.noreply.github.com>
2024-10-22 08:49:53 -0300

    .

--- main.js
+++ main.js
@@ -34,4 +34,33 @@ function verificarPalpite () {
         }
     }
 
+contagemPalpites++;
+campoPalpite.value = "";
+campoPalpite.focus();
+}
+
+envioPalpite.addEventListener('click', verificarPalpite);
+
+function finalizarJogo() {
+    campoPalpite.disabled = true;
+    envioPalpite.disabled = true;
+    botaoReiniciar = document.createElement('button');
+    document.body.appendChild(botaoReiniciar);
+    botaoReiniciar.addEventListener('click', reiniciarJogo);
+}
+
+
+function reiniciarJogo() {
+    contagemPalpites = 1;
+    const paragrafosReiniciar = document.querySelectorAll('.paragrafosResultado p');
+    for (const paragrafoReiniciar of paragrafosReiniciar) {
+        paragrafoReiniciar.textContent = "";
+ }
+ botaoReiniciar.parentNode.removeChild(botaoReiniciar);
+ campoPalpite.disabled = false;
+ envioPalpite.disabled = false;
+ campoPalpite.value = "";
+ campoPalpite.focus();
+ ultimoResultado.style.backgroundColor =  'white';
+ numeroAleatorio = Math.floor(Math.random() * 100) + 1;
 }
